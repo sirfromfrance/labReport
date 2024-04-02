12a837644ca2ec77641fd2e838dc05ddb4a44439
sirfromfrance <gritsenko5555@gmail.com>
2024-04-02 18:00:22 +0300

    update .js file

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-document.getElementById('lab1').addEventListener('click', function() {
-    var labContent = document.getElementById('labContent');
-    labContent.innerHTML = `
-        <h2>Лабораторна робота 1</h2>
-        <p>Тут буде відображена інформація про першу лабораторну роботу.</p>
-        <p>Варіанти:</p>
-        <ul>
-            <li>Тема 1</li>
-            <li>Тема 2</li>
-            <!-- Додайте сюди інші варіанти -->
-        </ul>
-    `;
-});
+const nav = document.getElementsByTagName("nav")[0];
+const sidebar = document.getElementsByTagName("aside")[0];
+const contentSpace = document.getElementsByTagName("content-space");
+fetch("structure.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("somthing went wrong with json structure");
+    }
+    return response.json();
+  })
+  .then((jsonData) => {
+    fillNav(jsonData);
+  })
+  .catch((error) => {
+    let navText = document.createElement("button");
+    navText.innerText = error;
+    navText.style.width = "100%";
+    nav.appendChild(navText);
+  });
+
+function fillNav(data) {
+  Object.entries(data).forEach(function ([button_id, button_data]) {
+    let navButton = document.createElement("button");
+    navButton.id = button_id;
+    navButton.innerText = button_data["name"];
+    nav.appendChild(navButton);
+    navButton.onclick=function(){
+        sidebar.innerHTML = "";
+        contentSpace.innerHTML="";
+        Object.entries(button_data["sections"]).forEach(function ([section_name, filename]){
+            let asideButton = document.createElement("button")
+            asideButton.innerHTML = section_name;
+            sidebar.append(asideButton);
+            asideButton.onclick=function(){
+                const full_path = button_data["path"] +filename;
+                fetch(full_path)
+                .then(response=>{
+                    if(!response.ok){
+                        throw new Error('something went wrong while uploading the file')
+                    }
+                    return response.text();
+                })
+                .then(htmlData=>{
+                    contentSpace.innerHTML=htmlData;
+                })
+                .catch(error=>{
+                    contentSpace.innerHTML=error;
+                })
+            }
+        })
+    }
+  });
+}
